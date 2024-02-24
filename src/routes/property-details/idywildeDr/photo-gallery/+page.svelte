902f3dc2c1fe6ff9edc0d9ea265716efe5d0202b
codeallstar <chad@codeallstar.com>
2024-02-23 21:21:39 -0500

    Add icons to carosuel

--- src/routes/property-details/idywildeDr/photo-gallery/+page.svelte
+++ src/routes/property-details/idywildeDr/photo-gallery/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	let elemCarousel: HTMLDivElement;
 	import photos from '$lib/images/properties/idywilde/large';
+	import { Icon } from 'svelte-materialdesign-icons';
 
 	function carouselLeft(): void {
 		const x =
@@ -17,33 +18,30 @@
 				: elemCarousel.scrollLeft + elemCarousel.clientWidth; // step right
 		elemCarousel.scroll(x, 0);
 	}
-
-	// Images
-	import photo1 from '$lib/images/properties/idywilde/large/002_img4468.jpg';
-	import photo2 from '$lib/images/properties/idywilde/large/001_img4468-twilight.jpg';
-	import photo3 from '$lib/images/properties/idywilde/large/003_img4471.jpg';
 </script>
 
-<section>
+<section class="container mx-auto m-2">
+	<ol class="breadcrumb">
+		<li class="crumb"><a class="anchor" href="/property-details/idywildeDr">Property Detail</a></li>
+		<li class="crumb-separator" aria-hidden>&rsaquo;</li>
+		<li>Photos</li>
+	</ol>
+</section>
+<section class="container mx-auto">
 	<div class="card p-4 grid grid-cols-[auto_1fr_auto] gap-4 items-center">
 		<!-- Button: Left -->
 		<button type="button" class="btn-icon variant-filled" on:click={carouselLeft}>
-			<i class="fa-solid fa-arrow-left" />
+			<Icon name="chevron-left" class="shrink-0 h-6 w-6 inline-flex" />
 		</button>
 		<!-- Full Images -->
 		<div bind:this={elemCarousel} class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto">
 			{#each photos as photo}
-				<img
-					class="snap-center w-full rounded-container-token"
-					src={photo}
-					alt={photo}
-					loading="lazy"
-				/>
+				<img class="snap-center rounded-container-token" src={photo} alt={photo} loading="lazy" />
 			{/each}
 		</div>
 		<!-- Button: Right -->
 		<button type="button" class="btn-icon variant-filled" on:click={carouselRight}>
-			<i class="fa-solid fa-arrow-right" />
+			<Icon name="chevron-right" class="shrink-0 h-6 w-6 inline-flex" />
 		</button>
 	</div>
 </section>
